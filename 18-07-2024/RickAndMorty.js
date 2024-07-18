945a5f1aaf86c7f4a0543c9892f24478da18d919
Luis David <david12.29@hotmail.com>
2024-07-18 10:28:11 -0500

    Lista la funcion apiRick

--- 18-07-2024/RickAndMorty.js
+++ 18-07-2024/RickAndMorty.js
@@ -3,8 +3,30 @@ const apiRick= async (pagina)=>{
     const api=await fetch(url);
     const data=await api.json();
     console.log(data);
-    
+    divRes=document.querySelector("#Resultado");
+    divRes.innerHTML=""
+    data.results.map(item=>{
+        divItem=document.createElement('div');
+        divItem.innerHTML=`
+            <div class="card">
+                <img src="${item.image}" alt="Imagen de ejemplo" class="card-img">
+                <div class="card-content">
+                    <h2 class="card-title">${item.name}</h2>
+                    <p class="card-text">Estatus:  ${item.status}</p>
+                    <p class="card-text">Especie:  ${item.species}</p>
+                    <p class="card-text">Ubicacion:  ${item.location.name}</p>
+                    <button class="btn" 
+                        data-name="${item.name}" 
+                        data-status="${item.status}" 
+                        data-species="${item.species}" 
+                        data-location="${item.location.name}" 
+                        data-image="${item.image}" 
+                        onClick='guardarCard(event)'>Guardar</button>
+                </div>
+            </div>
+        `;
+        divRes.appendChild(divItem);
+        
+    })
 }
 apiRick(1);
-
-
