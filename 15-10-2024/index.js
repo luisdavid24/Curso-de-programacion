2dca98f4e0660803700a6efbf2e4df24c3565c3e
Luis David Lopez Mendoza <david12.29@hotmail.com>
2024-10-15 19:20:07 -0500

    Listo la clase

---
+++ 15-10-2024/index.js
@@ -0,0 +1,231 @@
+/*
+Pregunta 1:
+¿Qué método agrega un elemento al final de un array en JavaScript?
+
+a) `unshift()`  
+b) `push()`  
+c) `pop()`  
+d) `shift()`  
+JULIAN
+*/
+
+/*
+Pregunta 2:
+Dado el siguiente código, ¿cuál será el contenido del array `frutas` después de ejecutar el siguiente código?
+
+```javascript
+let frutas = ['manzana', 'banana', 'naranja'];
+frutas.pop();
+```
+
+a) `['manzana', 'banana', 'naranja']`  
+b) `['manzana', 'banana']`  
+c) `['banana', 'naranja']`  
+d) `['naranja']`
+Sebastian
+*/
+
+
+/*
+Pregunta 3:
+¿Qué método elimina el primer elemento de un array en JavaScript?
+
+a) `pop()`  
+b) `shift()`  
+c) `unshift()`  
+d) `splice()`
+Juan andres
+*/
+
+
+/*Pregunta 4:
+Dado el siguiente código, ¿qué valor devolverá el método `slice()`?
+
+```javascript
+let numeros = [1, 2, 3, 4, 5];
+let subArray = numeros.slice(1, 3);
+console.log(subArray);
+```
+
+a) `[2, 3, 4]`  
+b) `[1, 2, 3]`  
+c) `[2, 3]`  
+d) `[1, 2]`
+
+
+*/
+
+/*
+Pregunta 5:
+¿Qué método se utiliza para agregar uno
+ o más elementos al principio de un array?
+
+a) `shift()`  
+b) `unshift()`  
+c) `push()`  
+d) `concat()`
+Matias
+*/
+
+/*
+Pregunta 6:
+¿Qué valor mostrará `console.log(arreglo)` después de 
+ejecutar el siguiente código?
+
+```javascript
+let arreglo = ['a', 'b', 'c', 'd', 'e'];
+arreglo.splice(2, 2);
+console.log(arreglo);
+```
+
+a) `['a', 'b', 'c', 'd']`  
+b) `['a', 'b', 'd', 'e']`  
+c) `['a', 'b', 'c', 'e']`  
+d) `['a', 'b', 'e']`
+Andres 
+*/
+
+
+/*
+Pregunta 7:
+¿Cuál es el propósito del método `concat()` en JavaScript?
+
+a) Unir los elementos de un array en una cadena  
+b) Combinar dos o más arrays en uno nuevo  
+c) Reemplazar elementos en un array  
+d) Invertir el orden de los elementos de un array  
+Maicol
+
+*/
+/*
+Pregunta 8:
+Dado el siguiente código, ¿qué se mostrará en la consola?
+
+```javascript
+let palabras = ['Hola', 'Mundo'];
+console.log(palabras.join('-'));
+```
+
+a) `"Hola Mundo"`  
+b) `"Hola,Mundo"`  
+c) `"Hola-Mundo"`  
+d) `"Hola-Mundo-"`
+
+*/
+
+
+
+/*
+Pregunta 9:
+¿Qué método invierte el orden de los 
+elementos de un array en JavaScript?
+
+a) `reverse()`  
+b) `sort()`  
+c) `splice()`  
+d) `concat()`
+Sebastian
+
+
+
+
+¿Qué valor devolverá el siguiente código?
+
+```javascript
+let numero = 5.6789;
+console.log(numero.toFixed(2));
+```
+
+a) `5.67`  
+b) `5.6789`  
+c) `5.68`  
+d) `5.6`
+
+
+
+---
+
+¿Cuál será el resultado de este código
+ utilizando `toPrecision()`?
+
+```javascript
+let num = 123.456;
+console.log(num.toPrecision(4));
+```
+
+a) `123.4`  
+b) `123.46`  
+c) `123.5`  
+d) `123.4`
+
+
+
+¿Qué valor devolverá el siguiente 
+código al usar `parseInt()`?
+
+```javascript
+let cadena = "42.89";
+console.log(parseInt(cadena));
+```
+
+a) `42.89`  
+b) `42`  
+c) `43`  
+d) `NaN`
+
+
+¿Qué resultado obtendremos al ejecutar 
+el siguiente código que utiliza `parseFloat()`?
+
+```javascript
+let str = "100.99px";
+console.log(parseFloat(str));
+```
+
+a) `100.99`  
+b) `100`  
+c) `NaN`  
+d) `99` Maicol
+
+¿Qué valor devuelve `isNaN()` en este código?
+
+```javascript
+let valor = "123abc";
+console.log(isNaN(valor));
+```
+
+a) `true`  
+b) `false`  
+c) `123`  
+d) `NaN`Juan Andres
+
+Dado el siguiente código, 
+¿qué valor aproximado devolverá `Math.random()`?
+
+```javascript
+console.log(Math.random());
+```
+
+a) Un número entre `0` y `100`  
+b) Un número entre `0` y `1`  
+c) Un número entre `1` y `10`  
+d) Un número entre `1` y `100`
+
+
+
+
+¿Qué resultado devuelve `Math.floor()`
+ en el siguiente código?
+
+```javascript
+let num = 7.89;
+console.log(Math.floor(num));
+```
+
+a) `7.8`  
+b) `7.9`  
+c) `7`  
+d) `8`
+
+
+*/
